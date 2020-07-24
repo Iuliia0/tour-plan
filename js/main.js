ef24eb523182b452817d164b3c2301b452747945
Iuliia0 <julia03951@gmail.com>
2020-07-24 19:43:41 +0300

    create: added map and booking

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
   },
 
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-
-
-})
+});
