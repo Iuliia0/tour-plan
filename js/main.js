ccbe47a2474b9b9f970e82e0c24440716f657cc6
Iuliia0 <julia03951@gmail.com>
2020-07-29 21:43:10 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -73,3 +73,9 @@ function init() {
 $('.newsletter').parallax({
   imageSrc: 'img/newsletter-bg.jpg'
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-button").classList.toggle("navbar-button--visible");
+});
